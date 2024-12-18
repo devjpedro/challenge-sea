9722da65d617cc78782347b7d7f8c6eea7c5880b
devjpedro <joaopedross.ti9@gmail.com>
2024-12-17 22:52:08 -0300

    (add): persisting redux states

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import ptBR from 'antd/locale/pt_BR'
 import { QueryClientProvider } from 'react-query'
 import { Provider as ReduxProvider } from 'react-redux'
 import { BrowserRouter } from 'react-router'
+import { PersistGate } from 'redux-persist/integration/react'
 import { ThemeProvider } from 'styled-components'
 
 import { Router } from './app/router'
 import { queryClient } from './lib/react-query'
-import { store } from './store'
+import { persistor, store } from './store'
 import { defaultTheme } from './styles/defaultTheme'
 import { GlobalStyle } from './styles/global'
 
@@ -15,28 +16,30 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReduxProvider store={store}>
-        <BrowserRouter>
-          <ThemeProvider theme={defaultTheme}>
-            <ConfigProvider
-              locale={ptBR}
-              theme={{
-                token: {
-                  colorPrimary: defaultTheme.colors.primaryColor,
-                },
-                components: {
-                  Layout: {
-                    bodyBg: defaultTheme.colors['gray-100'],
-                    siderBg: defaultTheme.colors['gray-100'],
-                    headerBg: defaultTheme.colors.white,
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <ThemeProvider theme={defaultTheme}>
+              <ConfigProvider
+                locale={ptBR}
+                theme={{
+                  token: {
+                    colorPrimary: defaultTheme.colors.primaryColor,
                   },
-                },
-              }}
-            >
-              <Router />
-            </ConfigProvider>
-            <GlobalStyle />
-          </ThemeProvider>
-        </BrowserRouter>
+                  components: {
+                    Layout: {
+                      bodyBg: defaultTheme.colors['gray-100'],
+                      siderBg: defaultTheme.colors['gray-100'],
+                      headerBg: defaultTheme.colors.white,
+                    },
+                  },
+                }}
+              >
+                <Router />
+              </ConfigProvider>
+              <GlobalStyle />
+            </ThemeProvider>
+          </BrowserRouter>
+        </PersistGate>
       </ReduxProvider>
     </QueryClientProvider>
   )
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,12 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import stepsReducer from './slices/stepsSlice'
 
+const persistConfig = {
+  key: 'steps',
+  storage,
+  whitelist: ['steps', 'activeStepId'],
+}
+
+const persistedStepsReducer = persistReducer(persistConfig, stepsReducer)
+
 export const store = configureStore({
   reducer: {
-    steps: stepsReducer,
+    steps: persistedStepsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
