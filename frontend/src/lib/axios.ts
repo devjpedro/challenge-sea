48de531ab5931560ac2b8ea9b522f1dd7d95e347
devjpedro <joaopedross.ti9@gmail.com>
2024-12-17 23:27:37 -0300

    (delete): removing enviroment variables from frontend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+// obs: estava utilizando variaveis de ambiente, mas por conta de algum bug na hora da criação da .env direto pelo comando no terminal, optei por deixar fixo para evitar bugs na hora do teste da aplicação, e deixar mais fácil de configurar o ambiente do projeto.
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'http://localhost:3333', // para rodar local
+  // baseURL: 'https://seachallengeapi.onrender.com', // para rodar em prod
 })
